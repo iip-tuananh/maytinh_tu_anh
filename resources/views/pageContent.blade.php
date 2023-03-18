4d3ee36e09278d865fe89b2ffd4bca14292761c6
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-18 20:01:03 +0700

    up

--- resources/views/pageContent.blade.php
+++ resources/views/pageContent.blade.php
@@ -47,7 +47,7 @@
        </div>
        <div class="row">
         <div class="col-md-6 col-xs-12">
-            {!!$setting->linkpopup!!}
+           <iframe src="{{$setting->linkpopup}}" frameborder="0"></iframe>
         </div>
         <div class="col-md-6 col-xs-12">
             {!!$setting->iframe_map!!}
