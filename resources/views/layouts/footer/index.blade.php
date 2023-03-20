4119ee2aa5a123ded8aa9214eafe57a8166dc92a
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-20 08:56:29 +0700

    up

--- resources/views/layouts/footer/index.blade.php
+++ resources/views/layouts/footer/index.blade.php
@@ -3,67 +3,58 @@
       <div class="footer-top">
          <div class="container">
             <div class="row">
+               <div class="col-md-6 col-xs-12">
+                  <h4 class="h4-cus"> Video about us</h4>
+                  {!!$setting->footer_content!!}
+               </div>
+               <div class="col-md-6 col-xs-12">
+                  <h4 class="h4-cus">MAP</h4>
+                  {!!$setting->iframe_map!!}
+               </div>
+            </div>
+            <div class="row">
                <div class="col-md-6 col-lg-4 mb-md-30px mb-lm-30px">
                   <div class="single-wedge">
-                     <h4 class="footer-herading">{{$setting->company}}</h4>
-                     <div class="need-help">
-                        <p class="phone-info">
-                           <span>
-                           {{$setting->phone1}} - {{$setting->phone2}}<br> <br>
+                    
+                     <div class="">
+                       <span style="display: flex;
+                       flex-wrap: nowrap;
+                       justify-content: center;
+                       align-items: center;">
+                    <img src="{{$setting->logo}}" alt="" srcset="" style="width:30%">
+                       </span><br>
+                       <h4 class="footer-herading">{{$setting->company}}</h4>
+                           <span style="text-align: center; width:100%" >
+                         {{$setting->webname}}
                            </span>
-                        </p>
-                     </div>
-                     <div>
-                        <p>
-                           CS1: 
-                           <span>
-                              {{$setting->address1}}
-                           </span>
-                        </p>
-                        <p>
-                           CS2: 
-                           <span>
-                              {{$setting->address2}}
-                           </span>
-                        </p>
+                  
                      </div>
                   </div>
                </div>
                <div class="col-md-6 col-lg-2 mb-md-30px mb-lm-30px">
                   <div class="single-wedge">
-                     <h4 class="footer-herading">Về chúng tôi</h4>
+                     <h4 class="footer-herading">Danh mục</h4>
                      <div class="footer-links">
                         <ul>
-                           @foreach ($pageContent as $item)
-                               @if($item->type == 've-chung-toi')
-                               <li><a href="{{route('pagecontent',['slug'=>$item->slug])}}">{{$item->title}}</a></li>
-                               @endif
+                           @foreach ($categoryhome as $cate)
+                              
+                               <li><a href="{{route('allListProCate',['danhmuc'=>$cate->slug])}}">-&nbsp;{{languageName($cate->name)}}</a></li>
+                          
                            @endforeach
                         </ul>
                            <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
-<!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-   _Hasync.push(['Histats.start', '1,4638260,4,300,113,63,00010011']);
-   _Hasync.push(['Histats.fasi', '1']);
-   _Hasync.push(['Histats.track_hits', '']);
-   (function() {
-   var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-   hs.src = ('//s10.histats.com/js15_as.js');
-   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-   })();</script>
-   <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4638260&101" alt="" border="0"></a></noscript>
-   <!-- Histats.com  END  -->
+
                      </div>
                   </div>
                </div>
                <div class="col-md-6 col-lg-2 mb-sm-30px mb-lm-30px">
                   <div class="single-wedge">
-                     <h4 class="footer-herading">Chính sách và điều khoản</h4>
+                     <h4 class="footer-herading">Chính sách </h4>
                      <div class="footer-links">
                         <ul>
                            @foreach ($pageContent as $item)
                                @if($item->type == 'ho-tro-khanh-hang')
-                               <li><a href="{{route('pagecontent',['slug'=>$item->slug])}}">{{$item->title}}</a></li>
+                               <li><a href="{{route('pagecontent',['slug'=>$item->slug])}}">-&nbsp;{{$item->title}}</a></li>
                                @endif
                            @endforeach
                         </ul>
@@ -72,19 +63,28 @@
                </div>
                <div class="col-md-6 col-lg-4 ">
                   <div class="single-wedge">
-                     <h4 class="footer-herading">Vị trí</h4>
-                     {!!$setting->iframe_map!!}
+                     <h4 class="footer-herading">Thông tin liên hệ</h4>
+                     <span><i class="fa-solid fa-phone"></i>&nbsp;{{$setting->phone1}}</span><br>
+                     @if($setting->phone2 != '')
+                     <span><i class="fa-solid fa-phone"></i>&nbsp;{{$setting->phone2s}}</span><br>
+                     @endif
+                     <span><i class="fa-solid fa-location-dot"></i></i>&nbsp;{{$setting->address1}}</span><br>
+                     @if ($setting->address2 != '')
+                        
+                     <span><i class="fa-solid fa-location-dot"></i></i>&nbsp;{{$setting->address2}}</span><br>
+                     @endif
                   </div>
                </div>
             </div>
+
          </div>
       </div>
       <div class="footer-bottom">
          <div class="container">
             <div class="row">
                <div class="col-md-6">
-                  <p class="copy-text"> © 2021 <strong>Rozer</strong> Made With <i class="fa fa-heart" style="color: red;" aria-hidden="true"></i> By <a class="company-name" href="">
-                     <strong> An Hưng</strong></a>.
+                  <p class="copy-text"> © 2021 <strong></strong> Made With <i class="fa fa-heart" style="color: red;" aria-hidden="true"></i> By <a class="company-name" href="">
+                     <strong>SBT</strong></a>.
                   </p>
                </div>
             </div>
--- resources/views/layouts/main/master.blade.php
+++ resources/views/layouts/main/master.blade.php
@@ -34,6 +34,7 @@
       <link rel="stylesheet" href="{{asset('frontend/css/style.min.css')}}">
       <link rel="stylesheet" href="{{asset('frontend/css/notify.css')}}">
       <link rel="stylesheet" href="{{asset('frontend/css/callbuttom.css')}}">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.6.0/tiny-slider.css">
       @yield('css')
      <!-- Messenger Plugin chat Code -->
